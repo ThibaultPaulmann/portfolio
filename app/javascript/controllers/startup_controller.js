e5390b92417a874e9d9d3996e37e244e96ef8a71
Thibault Paulmann <thibault.paulmann@gmail.com>
2022-09-14 14:54:39 +0200

    added transition animation between popup and main page

--- app/javascript/controllers/startup_controller.js
+++ app/javascript/controllers/startup_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="startup"
 export default class extends Controller {
-  static targets = ["popup"];
+  static targets = ["popup", "content"];
 
   connect() {
     console.log("connected to startup controller");
@@ -11,5 +11,6 @@ export default class extends Controller {
 
   remove() {
     this.popupTarget.classList.remove("active");
+    this.contentTarget.classList.add("fade-in");
   }
 }
